cecbade45c6b214cea214990379b9ecd2bf58828
mitchellurgero <mitchellurgero@gmail.com>
2016-01-08 15:52:01 -0600

    Begin adding VideoChat code (Skeleton code)
    Adding UPnP libs and VideoChat Skeleton code to prep for the next major
    release.

--- OpenMessage/videoChat.cs
+++ OpenMessage/videoChat.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NATUPNPLib;
+using System.Windows.Forms;
 
 namespace OpenMessage
 {
     class videoChat
     {
+        public string setupPort(string ipAddress, int port, String proto)
+        {
+           UPnPNATClass upnp = new UPnPNATClass();
+            IStaticPortMappingCollection maps = upnp.StaticPortMappingCollection;
+            foreach(IStaticPortMapping portMaps in maps)
+            {
+                
+                String IntIP = portMaps.InternalClient;
+                int IntPort = portMaps.InternalPort;
+                String intProto = portMaps.Protocol;
+                if(IntPort == port)
+                {
+                    maps.Remove(port, intProto);
+                    break;
+                }
+            }
+
+            try
+            {
+                maps.Add(port, proto, port, ipAddress, true, "Local Open Message Client");
+                return "true";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+            
+        }
+        
+
     }
 }
